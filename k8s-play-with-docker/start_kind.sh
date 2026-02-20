768d74c3b9dd2ea36463bb5d170f7a67ea6d337c
OlivierKouokam <62020582+OlivierKouokam@users.noreply.github.com>
2026-02-20 13:20:38 +0100

    Update start_kind.sh

--- k8s-play-with-docker/start_kind.sh
+++ k8s-play-with-docker/start_kind.sh
@@ -4,19 +4,23 @@ set -e
 export KUBECONFIG=/kind-cluster/kubeconfig
 
 if ! kind get clusters | grep -q mycluster; then
+
+    RANDOM_NAME="kind-$(head /dev/urandom | tr -dc a-z0-9 | head -c6)"
+
     echo "KinD Cluster Creation..."
-    kind create cluster --name eazy-cluster --config /kind-cluster/kind-config.yaml --kubeconfig /kind-cluster/kubeconfig
+    kind create cluster --name $RANDOM_NAME --config /kind-cluster/kind-config.yaml --kubeconfig /kind-cluster/kubeconfig
 
     echo "Attente API server..."
     until kubectl cluster-info >/dev/null 2>&1; do
         sleep 2
     done
-
+    
     echo "Attente Node Ready..."
     kubectl wait --for=condition=Ready node --all --timeout=120s
     echo
     echo "+++++++++++++++++++++++++++++++++++++"
     echo "Cluster Kubernetes-In-Docker started."
+    echo "Cluster name: $RANDOM_NAME"
     echo "+++++++++++++++++++++++++++++++++++++"
     echo
     echo "-----------------------------------------------------------"
